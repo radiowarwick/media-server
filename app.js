2673ef8be2db65ee3c92547c500512b7c1e98c2a
WillTheVideoMan <williamhall1324@gmail.com>
2019-04-11 15:17:23 +0100

    Completed Version 1. Included some starting data.

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const Koa = require("koa");
-const koaRouter = require("koa-router");
-const axios = require("axios");
-const fs = require("fs-extra");
-const crypto = require("crypto");
+const static = require("koa-static");
+const mount = require("koa-mount");
+const compress = require("koa-compress");
+const lastfm = require("./routes/lastfm");
 
 const app = new Koa();
-const router = new koaRouter();
 
-const endpoints = {
-  artist:
-    "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=e72f5e5eecceffa65784e10d18cfc5a5&format=json",
-  album:
-    "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=e72f5e5eecceffa65784e10d18cfc5a5&format=json"
-};
-
-const maxAge = 604800;
-
+/**
+ * For each async peice of middleware, try it, and report on any errors.
+ *
+ * Koa middleware cascades, so putting this at the top of the file ensures all errors are captured.
+ */
 app.use(async (ctx, next) => {
   try {
     await next();
@@ -26,88 +21,31 @@ app.use(async (ctx, next) => {
   }
 });
 
-router.get("/artist/:artist", async ctx => {
-  const artist = normalise(ctx.params.artist);
-  let hash = getHash(artist);
+/**
+ * Compress all compressable responses over 2KB.
+ */
+app.use(
+  compress({
+    threshold: 2048,
+    flush: require("zlib").Z_SYNC_FLUSH
+  })
+);
 
-  const file = await getFileInfo("./media/artist/" + hash + ".png", maxAge);
+/**
+ * Mount routes.
+ * - Lastfm uses a custom router.
+ * - Exec is static.
+ * - Video is static.
+ * - Shows is static.
+ */
+app.use(mount("/lastfm", lastfm.router.routes()));
+app.use(mount("/exec", static("./media/exec/")));
+app.use(mount("/video", static("./media/video/")));
+app.use(mount("/shows", static("./media/shows/")));
 
-  if (!file.exists || file.expired) {
-    console.log("visiting lastFM");
-    const response = await axios.get(endpoints.artist + "&artist=" + artist);
-    const data = response.data;
-
-    if (data.error) {
-      hash = "default";
-    } else {
-      const imgObj = data.artist.image.find(image => image.size === "large");
-      const path = "./media/artist/" + hash + ".png";
-      await downloadImage(imgObj["#text"], path, file.exists);
-    }
-  }
-
-  console.log("serving...");
-  const img = await fs.readFile("./media/artist/" + hash + ".png");
-
-  ctx.set("Content-Type", "image/png");
-  ctx.set("Cache-Control", "max-age=" + maxAge);
-  ctx.body = img;
-});
-
-app.on("error", (err, ctx) => {
-  console.log(err.message);
-});
-
-app.use(router.routes());
-
-app.listen(3000);
+/**
+ * Start the lastFM image cache pruner.
+ */
+lastfm.pruner.start();
 
 module.exports = app;
-
-function normalise(string) {
-  return string
-    .trim()
-    .toLowerCase()
-    .replace(/\s{2,}/g, " ")
-    .replace(/\s/g, "+");
-}
-
-function getHash(string) {
-  return crypto
-    .createHash("md5")
-    .update(string)
-    .digest("hex");
-}
-
-async function getFileInfo(path, maxAge) {
-  return new Promise(resolve => {
-    console.log("check");
-    fs.stat(path, (err, stats) => {
-      if (err) resolve({ exists: false, expired: false });
-      else if (stats.birthtimeMs + maxAge * 1000 < Date.now())
-        resolve({ exists: true, expired: true });
-      else resolve({ exists: true, expired: false });
-    });
-  });
-}
-
-async function downloadImage(imgURL, path, exists) {
-  console.log("downloading image");
-
-  if (exists) await fs.unlink(path);
-
-  const writer = fs.createWriteStream(path);
-
-  const imgResponse = await axios({
-    url: imgURL,
-    method: "GET",
-    responseType: "stream"
-  });
-
-  imgResponse.data.pipe(writer);
-
-  return new Promise((resolve, reject) => {
-    writer.on("finish", resolve);
-    writer.on("error", reject);
-  });
-}
---
+++ routes/lastfm.js
@@ -0,0 +1,288 @@
+const koaRouter = require("koa-router");
+const CronJob = require("cron").CronJob;
+const crypto = require("crypto");
+const axios = require("axios");
+const fs = require("fs-extra");
+
+const imagemin = require("imagemin");
+const imageminPngquant = require("imagemin-pngquant");
+
+const router = new koaRouter();
+
+const apiKey = "e72f5e5eecceffa65784e10d18cfc5a5";
+
+const endpoints = {
+  artist: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json",
+  track: "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&format=json"
+};
+
+/**
+ * Get an artist's image. This is usually a candid shot of the artist infront of a tree.
+ */
+router.get("/artist/:artist", async ctx => {
+  /**
+   * Gets the artist string and generates a hash based on it.
+   */
+  const artist = escapeString(ctx.params.artist);
+  const hash = getHash(artist);
+
+  /**
+   * A boolean value which determines if the image for the artist exists in our local disk.
+   */
+  const exists = await fs.pathExists("./media/artist/" + hash + ".png");
+
+  /**
+   * The base filename. Hopefully it will get overwritten. If not, a default image will be served.
+   */
+  let filename = "default.png";
+
+  /**
+   * If the image does not exist on local disk, the query the lastfm api to get the artist info.
+   * Else, proceed.
+   */
+  if (!exists) {
+    const response = await axios.get(endpoints.artist, {
+      params: { artist: artist, api_key: apiKey }
+    });
+
+    /**
+     * If lastfm returns an error, then the artist does not exist on their systems.
+     * Else, proceed.
+     */
+    if (!response.data.error && response.data.artist) {
+      /**
+       * Retrive the image URL from the response.
+       */
+      const imgURL = findImageURL(response.data.artist.image);
+
+      /**
+       * If the image is null, then lastfm has no image for this artist.
+       * Else, proceed.
+       */
+      if (imgURL !== "") {
+        /**
+         * Set the filename to the new artist hash. All images are PNGs.
+         */
+        filename = hash + ".png";
+
+        /**
+         * Save the artist image to local disk.
+         */
+        await saveImage(imgURL, "./media/artist/", filename, exists);
+      }
+    }
+  } else {
+    /**
+     * If the artist image exists, set the filename to the hash value.
+     */
+    filename = hash + ".png";
+  }
+
+  /**
+   * Serve the artist image.
+   */
+  ctx.set("Content-Type", "image/png");
+  ctx.set("Cache-Control", "max-age=1209600");
+  ctx.body = await fs.readFile("./media/artist/" + filename);
+});
+
+/**
+ * Get a track's image. This is usually the album art.
+ */
+router.get("/track/:artist/:track", async ctx => {
+  /**
+   * Get the artist and track from the request and hash them together.
+   */
+  const artist = escapeString(ctx.params.artist);
+  const track = escapeString(ctx.params.track);
+  const hash = getHash(artist + "/" + track);
+
+  /**
+   * A boolean value which determines if the image for the track exists in our local disk.
+   */
+  const exists = await fs.pathExists("./media/track/" + hash + ".png");
+
+  /**
+   * The base filename. Hopefully it will get overwritten. If not, a default image will be served.
+   */
+  let filename = "default.png";
+
+  /**
+   * If the image does not exist on local disk, the query the lastfm api to get the track info.
+   * Else, proceed.
+   */
+  if (!exists) {
+    const response = await axios.get(endpoints.track, {
+      params: { artist: artist, track: track, api_key: apiKey }
+    });
+
+    /**
+     * If lastfm returns an error or a null response, then the track and info does not exist on their systems.
+     * Else, proceed.
+     */
+    if (!response.data.error && response.data.track.album) {
+      /**
+       * Retrive the image URL from the response.
+       */
+      const imgURL = findImageURL(response.data.track.album.image);
+
+      /**
+       * If the image is null, then lastfm has no image for this track.
+       * Else, proceed.
+       */
+      if (imgURL !== "") {
+        /**
+         * Set the filename to the new track hash. All images are PNGs.
+         */
+        filename = hash + ".png";
+
+        /**
+         * Save the track image to local disk.
+         */
+        await saveImage(imgURL, "./media/track/", filename, exists);
+      }
+    }
+  } else {
+    /**
+     * If the track image exists, set the filename to the hash value.
+     */
+    filename = hash + ".png";
+  }
+
+  /**
+   * Serve the track image.
+   */
+  ctx.set("Content-Type", "image/png");
+  ctx.set("Cache-Control", "max-age=1209600");
+  ctx.body = await fs.readFile("./media/track/" + filename);
+});
+
+/**
+ * Returns an escaped string.
+ * - Removes leading and trailing whitespace.
+ * - All chars to lower case.
+ * - Ensures single spaced words.
+ */
+const escapeString = str =>
+  str
+    .trim()
+    .toLowerCase()
+    .replace(/\s{2,}/g, " ");
+
+/**
+ * Generates a MD5 hash of a string. Hex encoded.
+ *
+ * @param {string} str - String to be hashed.
+ */
+const getHash = str =>
+  crypto
+    .createHash("md5")
+    .update(str)
+    .digest("hex");
+
+/**
+ * Finds and returns the correct sized image URL from an object.
+ *
+ * @param {*} obj - the object to be searched. Format defined by lastfm API.
+ */
+const findImageURL = obj => {
+  const img = obj.find(image => image.size === "large");
+  return img["#text"];
+};
+
+/**
+ * Saves an image from a remote location to local disk of a given path.
+ *
+ * Destroys existing file in path if marked as existing.
+ *
+ * @param {string} imgURL - The remote URL to fetch.
+ * @param {string} dir - The dir to save the remote URL.
+ * @param {string} filename - The filename of the file to save the remote URL.
+ * @param {bool} exists - Defines if there is a file already in the path.
+ */
+const saveImage = async (imgURL, dir, filename, exists) => {
+  /**
+   * Build the path string.
+   */
+  const path = dir + "/" + filename;
+
+  /**
+   * If the file exists, destroy it.
+   *
+   * Gives a new birthtime. Allows for pruning (see prune).
+   */
+  if (exists) await fs.unlink(path);
+
+  /**
+   * Define a new file writer at the given path.
+   */
+  const writer = fs.createWriteStream(path);
+
+  /**
+   * Get the remote image, streaming the response to the writer.
+   */
+  const imgResponse = await axios({
+    url: imgURL,
+    method: "GET",
+    responseType: "stream"
+  });
+
+  imgResponse.data.pipe(writer);
+
+  /**
+   * Returns a promise that resolves when writing and compression is complete.
+   */
+  return new Promise((resolve, reject) => {
+    writer.on("finish", async () => {
+      /**
+       * Compresses the recieved png, roughly quartering the file size.
+       */
+      await imagemin([path], dir, {
+        plugins: [imageminPngquant({ quality: [0.6, 0.8] })]
+      });
+      resolve();
+    });
+    writer.on("error", reject);
+  });
+};
+
+/**
+ * Prunes the images at midnight every day, deleting images older than 14 days.
+ */
+const pruner = new CronJob("0 0 0 * * *", async () => {
+  /**
+   * Defines the directories to search in.
+   */
+  const dirs = ["./media/artist/", "./media/track/"];
+
+  /**
+   * For each directory, fetch the images within it.
+   */
+  await dirs.forEach(async dir => {
+    const images = await fs.readdir(dir);
+
+    /**
+     * For each image in the directory, delete it if the image is older than 1209600000 milliseconds (14 days).
+     *
+     * Will not delete the default images.
+     */
+    images.forEach(async image => {
+      if (image !== "default.png") {
+        await fs.stat(dir + image, async (err, stats) => {
+          if (!err) {
+            if (stats.birthtimeMs + 1209600000 < Date.now()) {
+              await fs.unlink(dir + image);
+            }
+          }
+        });
+      }
+    });
+  });
+});
+
+//
+
+/**
+ * Export the routes and pruner.
+ */
+module.exports = { router: router, pruner: pruner };
